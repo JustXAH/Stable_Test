59530e83c401f37a39fab00cb5ea5dea89df24d3
JustXAH <justxah@gmail.com>
2022-10-13 14:02:17 +0300

    Made Metamask connect button.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DAppProvider } from "@usedapp/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <DAppProvider config={{}} >
+        <Component {...pageProps} />
+      </DAppProvider>
+  )
 }
 
 export default MyApp
